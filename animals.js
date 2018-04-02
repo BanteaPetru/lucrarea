f65a7d4f3ccd299590f583a709f6e20f9aeef359
BanteaPetru <36608401+BanteaPetru@users.noreply.github.com>
2018-04-02 18:48:18 +0300

    es6

--- animals.js
+++ animals.js
@@ -1,13 +1,5 @@
-'use strict';
-
-var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
-
-function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
-
-var Animals = function () {
-    function Animals(options) {
-        _classCallCheck(this, Animals);
-
+class Animals{
+    constructor(options){
         this.rasa = options.rasa;
         this.culoare = options.culoare;
         this.varsta = options.varsta;
@@ -15,40 +7,33 @@ var Animals = function () {
         this.energ = options.energ;
         this.oboseala = options.oboseala;
     }
-
-    _createClass(Animals, [{
-        key: 'mananca',
-        value: function mananca() {
-            if (this.infom < 7) {
-                console.log(this.rasa + ' ' + this.culoare + ' ' + 'cu varsta de' + ' ' + this.varsta + ' ' + 'Mananca');
-            } else {
-                console.log('Animalul nu este flamand');
-            }
+    mananca(){
+        if (this.infom < 7) {
+            console.log(this.rasa + ' ' + this.culoare + ' ' + 'cu varsta de' + ' ' + this.varsta + ' ' + 'Mananca');
         }
-    }, {
-        key: 'semisca',
-        value: function semisca() {
-            if (this.energ > 6) {
-                console.log(this.rasa + ' ' + this.culoare + ' ' + 'cu varsta de' + ' ' + this.varsta + ' ' + 'Se misca');
-            } else {
-                console.log(this.rasa + ' ' + 'trebuie sa doarma');
+        else{
+            console.log('Animalul nu este flamand');
             }
+    }
+    semisca(){
+        if(this.energ > 6){
+           console.log(this.rasa + ' ' + this.culoare + ' ' + 'cu varsta de' + ' ' + this.varsta + ' ' + 'Se misca');
         }
-    }, {
-        key: 'doarme',
-        value: function doarme() {
-            if (this.oboseala > 5) {
-                console.log(this.rasa + ' ' + this.culoare + ' ' + 'cu varsta de' + ' ' + this.varsta + ' ' + 'Doarme');
-            } else {
-                console.log(this.rasa + ' ' + 'are destula energie pentru a se mai misca');
-            }
+        else{
+            console.log(this.rasa + ' ' + 'trebuie sa doarma');
         }
-    }]);
+    }
+     doarme(){
+        if (this.oboseala > 5 ) {
+            console.log(this.rasa + ' ' + this.culoare + ' ' + 'cu varsta de' + ' ' + this.varsta + ' ' + 'Doarme');
+        }
+        else{
+            console.log(this.rasa + ' ' + 'are destula energie pentru a se mai misca');
+        }
+    }
+}
 
-    return Animals;
-}();
-
-var animal = new Animals({
+let animal = new Animals({
     rasa: 'Tigrul',
     culoare: 'Alb',
     varsta: '10 ani',
